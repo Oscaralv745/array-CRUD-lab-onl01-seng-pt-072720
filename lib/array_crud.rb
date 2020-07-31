af7c9bdd62560278816378489f1574cfad760b36
Oscar Alvarez <gavinalv6@gmail.com>
2020-07-31 16:18:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  cats = [judas, oliver, luna, simba]
+  cats = ["Cucumbers", "oliver", "luna", "simba"]
 end
 
 def add_element_to_end_of_array(array, element)

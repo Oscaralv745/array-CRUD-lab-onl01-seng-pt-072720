61073c184860ede7d06ce694ecff5bae5d1768c8
Oscar Alvarez <gavinalv6@gmail.com>
2020-07-31 16:33:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   cats = ["Cucumbers", "oliver", "luna", "simba", "arrays!"]
-  cats.pop ("arrays!")
+  cats.pop
 end
 
 def remove_element_from_start_of_array(array)

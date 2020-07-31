f5085ff0dd0f187d84d24a482c96151a85dbeb1a
Oscar Alvarez <gavinalv6@gmail.com>
2020-07-31 16:27:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   cats = ["Cucumbers", "oliver", "luna", "simba"]
-  cats.push ("Judas")
+  cats.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
